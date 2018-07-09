f544483a08d678b933f3990653e161fdb2b3d9d2
Simon Hutchinson <si@springyweb.com>
2018-07-09 14:50:32 +0100

    Combine zookeeper with NiFi

--- build_environment.sh
+++ build_environment.sh
@@ -4,4 +4,5 @@ echo "Building for environment $1"
 
 rm -rf deployment || true
 
-vortex --template templates --output deployment -varpath ./environments/$1.yaml
+vortex --template templates/zk --output deployment/zk -varpath ./environments/$1.yaml
+vortex --template templates/nifi --output deployment/nifi -varpath ./environments/$1.yaml
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+kubectl create ns zookeeper
+kubectl create -f deployment/zk/poddisruptionbudget.yaml --namespace=zookeeper
+kubectl create -f deployment/zk/statefulset.yaml --namespace=zookeeper
+kubectl create -f deployment/zk/service.yaml --namespace=zookeeper
+
 kubectl create ns nifi
-kubectl create -f deployment/service.yaml --namespace=nifi
-kubectl create -f deployment/statefulset.yaml --namespace=nifi
+kubectl create -f deployment/nifi/service.yaml --namespace=nifi
+kubectl create -f deployment/nifi/statefulset.yaml --namespace=nifi
